d1dc428e380c2a59156395c40b656c226a9c825e
Ashok Nayar <nayara@onid.oregonstate.edu>
2015-03-10 10:14:13 -0700

    added roles, starting locations

---
+++ roles.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="nayara-final.css">
+<script src="final.js"></script>
+
+    <!-- Custom styles for this template -->
+</head>
+
+<body>
+  	<?php
+        echo 'Add a new Role<br>';
+        echo '<label>Role: <input type="text" id="role" name="role"></label><br>';
+ 		echo '<button onclick="addRole()"> Add Role</button><br>';    
+        echo '<div id="status"></div><br><br>';
+        echo '<div id="roles"></div>';
+  	?>
+</body>
+</html>
+
